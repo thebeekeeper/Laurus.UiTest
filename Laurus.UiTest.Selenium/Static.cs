17b20499c607409626fcb650d6c54949236b91d2
Nick Gamroth <thebeekeeper@gmail.com>
2013-11-13 14:52:09 -0600

    added static component

---
+++ Laurus.UiTest.Selenium/Static.cs
@@ -0,0 +1,22 @@
+﻿using Laurus.UiTest.Controls;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laurus.UiTest.Selenium
+{
+	public class Static : BaseControl, IStatic
+	{
+		public Static(IWebDriver driver)
+			: base(driver)
+		{ }
+
+		string IStatic.Text
+		{
+			get { return GetNative().Text; }
+		}
+	}
+}
